cee47d354a08fcc3bcdea8122547ed7544ad452a
Ali hassan <alihgad2@gmail.com>
2026-05-10 23:16:49 +0300

    firebase

--- dist/common/enums/multer.enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MulterEnum = void 0;
-var MulterEnum;
-(function (MulterEnum) {
-    MulterEnum[MulterEnum["memoryStorage"] = 0] = "memoryStorage";
-    MulterEnum[MulterEnum["diskStorage"] = 1] = "diskStorage";
-})(MulterEnum || (exports.MulterEnum = MulterEnum = {}));
--- dist/common/exceptions/success.responce.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SuccessResponce = void 0;
-const SuccessResponce = ({ res, message = 'success', status = 200, data }) => {
-    return res.status(status).json({
-        message, data
-    });
-};
-exports.SuccessResponce = SuccessResponce;
